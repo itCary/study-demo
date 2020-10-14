a3f7f7ae8fcd8334a361496094d9287e163a1208
liuchenglong <liuchenglong@i-join.cc>
2020-10-14 20:03:16 +0800

    2020-10-14 20:03

--- demo/test/src/test/java/online/goudan/QueueTest.java
+++ demo/test/src/test/java/online/goudan/QueueTest.java
@@ -1,9 +1,11 @@
 package online.goudan;
 
-import online.goudan.domain.Car;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chenglongliu
@@ -11,6 +13,7 @@ import java.util.concurrent.ArrayBlockingQueue;
  * @desc 队列的使用
  */
 public class QueueTest {
+    int i = 0;
 
     /**
      * 有界队列的使用
@@ -18,9 +21,47 @@ public class QueueTest {
     @Test
     public void test01() throws Exception {
         ArrayBlockingQueue<String> strings = new ArrayBlockingQueue<String>(20);
-        for (int i = 0; i < 20; i++) {
-            strings.put("" + i);
+        for (; i < 20; i++) {
+            strings.put(String.valueOf(i));
         }
         System.out.println(strings);
+        boolean flag = false;
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(1, 1, 5, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(1));
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                    try {
+                        if (i % 3 == 0) {
+                            Thread.sleep(2500L);
+                            strings.put(String.valueOf(i++));
+                        } else {
+                            Thread.sleep(1000L);
+                            strings.put(String.valueOf(i++));
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        while (true) {
+            String s = strings.take();
+            if (s != null) {
+                System.out.printf("time:%.2f,字符:%s%n", System.currentTimeMillis() % 1000000 / 1000f, s);
+            } else {
+                System.out.printf("time:%.2f,字符:%s%n", System.currentTimeMillis() % 1000000 / 1000f, s);
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void test02(){
+        Date date = new Date(1478188800000L);
+        System.out.println(date);
+        Date date1 = new Date(1530544308000L);
+        System.out.println(date1);
     }
 }
--- demo/test/src/test/java/online/goudan/ThreadPoolTest.java
+++ demo/test/src/test/java/online/goudan/ThreadPoolTest.java
@@ -1,13 +1,11 @@
 package online.goudan;
 
 import javafx.concurrent.Worker;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author chenglongliu
@@ -15,10 +13,13 @@ import java.util.concurrent.TimeUnit;
  * @desc 线程池的使用
  */
 public class ThreadPoolTest {
-    @Test
-    public void test01() throws Exception {
-        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(3, 20, 10, TimeUnit.MILLISECONDS,
-                new LinkedBlockingDeque<>(), new ThreadFactory() {
+
+    private ThreadPoolExecutor poolExecutor;
+
+    @Before
+    public void init() throws Exception {
+        poolExecutor = new ThreadPoolExecutor(3, 20, 10, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(300), new ThreadFactory() {
             int threadNum = 1;
 
             @Override
@@ -29,6 +30,11 @@ public class ThreadPoolTest {
                 return thread;
             }
         });
+    }
+
+    @Test
+    public void test01() throws Exception {
+
         for (int i = 0; i < 300; i++) {
             poolExecutor.execute(new Runnable() {
                 @Override
@@ -44,6 +50,40 @@ public class ThreadPoolTest {
         }
         Thread.sleep(Integer.MAX_VALUE);
 
+    }
+
+    private volatile boolean flag;
+
+    /**
+     * volatile 只能保证可见性,不能保证原子性和有序性
+     * @throws Exception
+     */
+    @Test
+    public void test02() throws Exception {
+        flag = true;
+        poolExecutor.execute(() -> {
+            while (flag) {
+                try {
+                    Thread.sleep(3000L);
+                    System.out.printf("%s,%d,%b%n", Thread.currentThread().getName(), System.currentTimeMillis(), flag);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        poolExecutor.execute(() -> {
+            try {
+                Thread.sleep(10 * 1000);
+                flag = false;
+                System.out.printf("%s,%d,%b%n", Thread.currentThread().getName(), System.currentTimeMillis(), flag);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread.sleep(Integer.MAX_VALUE);
     }
 
 
