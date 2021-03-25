f78feb55eff8314718c6d389fbd24c3dd48602fe
liuchenglong <liuchenglong@i-join.cc>
2021-03-25 17:01:01 +0800

    2021-03-25 17:00

--- MQ-demo/rabbitmq-util/src/main/java/online/goudan/util/ConnectionUtil.java
+++ MQ-demo/rabbitmq-util/src/main/java/online/goudan/util/ConnectionUtil.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeoutException;
  * @desc 获取connection的封装
  */
 public class ConnectionUtil {
-    private static String host = "172.16.239.131";
+    private static String host = "172.16.239.130";
     private static int port = 5672;
     private static String username = "goudan";
     private static String password = "goudan125";
